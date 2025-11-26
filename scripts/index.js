21b7834ccda74282effa7ae6885c866914785b39
Manvir Sidhu <Pokemonstar5001@gmail.com>
2025-11-26 02:17:29 -0500

    Spots 5 test

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,32 @@
 const editProfileBtn = document.querySelector(".profile__edit-button");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
 
 const newPostBtn = document.querySelector(".profile__add-button");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
+const newCardForm = newPostModal.querySelector(".modal__form");
+const newCardImageInput = newPostModal.querySelector("#card-image-input");
+const newCardDescriptionInput = newPostModal.querySelector(
+  "#card-description-input"
+);
+
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const cardImage = document.querySelector(".card__image");
+const cardTitle = document.querySelector(".card__title");
+
 editProfileBtn.addEventListener("click", function () {
+  editProfileNameInput.value = profileName.textContent;
+  editProfileDescriptionInput.value = profileDescription.textContent;
   editProfileModal.classList.add("modal_is-opened");
 });
 
@@ -21,3 +41,20 @@ newPostBtn.addEventListener("click", function () {
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
 });
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = newProfileNameInput.value;
+  profileDescription.textContent = newProfileDescriptionInput.value;
+}
+
+newCardForm.addEventListener("submit", handleProfileFormSubmit);
+
+function handleCardFormSubmit(evt) {
+  evt.preventDefault();
+  console.log(newCardImageInput.value);
+  console.log(newCardDescriptionInput.value);
+  newPostModal.classList.remove("modal_is-opened");
+}
+
+newCardForm.addEventListener("submit", handleCardFormSubmit);
